7a9bb9c77beb8020df18ed6aa3f1064fed83e620
Caio Gandra <caio@digicash.tv>
2021-04-26 00:32:58 -0300

    Created moderation service

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -19,7 +19,8 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, content });
+    const initialStatus = 'pending';
+    comments.push({ id: commentId, content, status: initialStatus });
 
     commentsByPostId[req.params.id] = comments;
 
@@ -28,6 +29,7 @@ app.post('/posts/:id/comments', async (req, res) => {
         data: {
             id: commentId,
             content,
+            status: initialStatus,
             postId: req.params.id
         }
     });
@@ -35,8 +37,31 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Received Event', req.body.type);
+
+    const { type, data } = req.body;
+
+    if(type === "CommentModerated") {
+        const { postId, id, content, status} = data;
+        const comments = commentsByPostId[postId] || [];
+
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+        type: 'CommentUpdated',
+        data: {
+            id,
+            content,
+            status,
+            postId
+        }
+    });
+    }
 
     res.send({});
 });
